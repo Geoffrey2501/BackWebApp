e746b6db49cff50f2116ec819d8693c1eb0109a7
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2026-02-19 16:27:31 +0100

    ajout du cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask_cors import CORS  # Importer l'extension
 from app.config import config_by_name
 
 
@@ -7,7 +7,11 @@ def create_app(config_name: str = "dev") -> Flask:
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
 
-    from app.routes import register_blueprints
+    # Activer CORS pour toutes les routes
+    # En production, il est recommandé de restreindre origins=["http://ton-domaine-front.fr"]
+    CORS(app)
 
+    from app.routes import register_blueprints
     register_blueprints(app)
+
     return app
